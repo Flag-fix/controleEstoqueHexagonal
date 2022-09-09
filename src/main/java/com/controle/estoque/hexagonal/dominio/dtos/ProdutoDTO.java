0d4068f7cafef0b17cc83e5b9dba55c8fd608b9c
flag-fix <isac.bandeir4@gmail.com>
2022-09-09 19:26:49 -0300

    Envio atividade hexagonal

--- src/main/java/com/controle/estoque/hexagonal/dominio/dtos/ProdutoDTO.java
+++ src/main/java/com/controle/estoque/hexagonal/dominio/dtos/ProdutoDTO.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NegativeOrZero;
 import javax.validation.constraints.NotEmpty;
 
@@ -14,30 +15,30 @@ import javax.validation.constraints.NotEmpty;
 @NoArgsConstructor
 public class ProdutoDTO {
 
-//    @Schema(
-//            example = "Pilha",
-//            description = "Identificação do nome do produto",
-//            required = true
-//    )
-//    @JsonProperty("nome")
-//    @NotEmpty(message = "Nome não pode ser nulo ou vazio")
+    @Schema(
+            example = "Pilha",
+            description = "Identificação do nome do produto",
+            required = true
+    )
+    @JsonProperty("nome")
+    @NotEmpty(message = "Nome não pode ser nulo ou vazio")
     private String nome;
 
-//    @Schema(
-//            example = "50.00",
-//            description = "Identificação do preço do produto",
-//            required = true
-//    )
-//    @JsonProperty("preco")
-//    @NegativeOrZero(message = "O preço de um produto não pode ser negativo ou zero")
+    @Schema(
+            example = "50.00",
+            description = "Identificação do preço do produto",
+            required = true
+    )
+    @JsonProperty("preco")
+    @Min(value = 1, message = "O preço de um produto não pode ser negativo ou zero")
     private Double preco;
 
-//    @Schema(
-//            example = "10",
-//            description = "Identificação do quantidade do produto",
-//            required = true
-//    )
-//    @JsonProperty("quantidade")
-//    @NegativeOrZero(message = "A quantidade do produto não pode ser negativo ou zero")
+    @Schema(
+            example = "10",
+            description = "Identificação do quantidade do produto",
+            required = true
+    )
+    @JsonProperty("quantidade")
+    @Min(value = 1, message = "A quantidade do produto não pode ser negativo ou zero")
     private Integer quantidade;
 }
--- src/test/java/com/controle/estoque/hexagonal/aplicacao/adaptatores/controllers/ProdutoControllerTest.java
+++ src/test/java/com/controle/estoque/hexagonal/aplicacao/adaptatores/controllers/ProdutoControllerTest.java
@@ -20,8 +20,23 @@ public class ProdutoControllerTest extends TestRepositoryIT {
 
         ProdutoDTO produtoDTO = new ProdutoDTO();
         produtoDTO.setNome("Calça Jeans");
-        produtoDTO.setQuantidade(1);
-        produtoDTO.setPreco(10.0);
+        produtoDTO.setQuantidade(10);
+        produtoDTO.setPreco(-10.0);
+
+        try {
+            controller.criarProduto(produtoDTO);
+        }catch (Exception e){
+            assertEquals("criarProduto.produtoDTO.preco: O preço de um produto não pode ser negativo ou zero", e.getMessage());
+        }
+    }
+
+    @Test
+    public void criandoProduto() {
+
+        ProdutoDTO produtoDTO = new ProdutoDTO();
+        produtoDTO.setNome("Calça Jeans");
+        produtoDTO.setQuantidade(10);
+        produtoDTO.setPreco(10.00);
 
         controller.criarProduto(produtoDTO);
         var produto = super.findFirst(ProdutoEntity.class);
